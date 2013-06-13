33216bdcf44f0ecb836fda296d7ef10ebd5bb44f
void <git.email.address@googlemail.com>
2013-06-13 09:29:27 +0200

    The basic IPA bot works

---
+++ LinguisticsBot/Bot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using BIRCh;
+
+namespace LinguisticsBot
+{
+	class Bot : Client
+	{
+		delegate void MatchHandler(Match match, User user, string target, string message);
+
+		List<string> Channels;
+		Dictionary<string, MatchHandler> MatchHandlers;
+
+		public Bot(List<string> channels)
+		{
+			Channels = channels;
+			MatchHandlers = new Dictionary<string, MatchHandler>();
+			MatchHandlers["^\\.ipa (.+)$"] = OnIPA;
+		}
+
+		protected override void OnConnect()
+		{
+			Console.WriteLine("Connected");
+		}
+
+		protected override void OnDisconnect(Exception exception)
+		{
+			Console.WriteLine("Disconnected: {0}", exception.Message);
+		}
+
+		protected override void OnReceive(string line)
+		{
+			Console.WriteLine("< {0}", line);
+		}
+
+		protected override void OnSend(string line)
+		{
+			Console.WriteLine("> {0}", line);
+		}
+
+		protected override void OnEntry()
+		{
+			foreach (string channel in Channels)
+				JoinChannel(channel);
+		}
+
+		protected override void OnMessage(User user, string target, string message)
+		{
+			Console.WriteLine("[{0}] <{1}@{2}> {3}", target, user.Nick, user.Host, message);
+			foreach (var pair in MatchHandlers)
+			{
+				Match match = Regex.Match(message, pair.Key);
+				if (match.Success)
+				{
+					pair.Value(match, user, target, message);
+					break;
+				}
+			}
+		}
+
+		void Respond(User user, string target, string line)
+		{
+			bool isPrivateMessage = target.Length > 0 && target[0] != '#';
+			if (isPrivateMessage)
+				SendMessage(user.Nick, line);
+			else
+				SendMessage(target, line);
+		}
+
+		void OnIPA(Match match, User user, string target, string message)
+		{
+			string translation = XSAMPA.TranslateSentence(match.Groups[1].Value);
+			Respond(user, target, translation);
+		}
+	}
+}
--- LinguisticsBot/Program.cs
+++ LinguisticsBot/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace LinguisticsBot
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace LinguisticsBot
 {
 	class Program
 	{
 		static void Main(string[] arguments)
 		{
+			if (arguments.Length < 5)
+			{
+				Console.WriteLine("Usage:");
+				Console.WriteLine("<server> <port> <nick> <user> <real name> <channels to join>");
+				return;
+			}
+			string server = arguments[0];
+			int port = Convert.ToInt32(arguments[1]);
+			string nick = arguments[2];
+			string user = arguments[3];
+			string realName = arguments[4];
+			List<string> channels = arguments.Skip(5).ToList();
+			IPAddress[] addresses = Dns.GetHostAddresses(server);
+			IPEndPoint endPoint = new IPEndPoint(addresses[0], port);
+			Bot bot = new Bot(channels);
+			bot.Nick = nick;
+			bot.User = user;
+			bot.RealName = realName;
+			bot.Run(endPoint);
 		}
 	}
 }
