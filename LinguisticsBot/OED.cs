8b0a06b005cde80e5415d08fea9a9db601cef6cd
void <git.email.address@googlemail.com>
2013-11-02 00:52:13 +0100

    Minor OED fix

--- LinguisticsBot/OED.cs
+++ LinguisticsBot/OED.cs
@@ -11,11 +11,11 @@ namespace LinguisticsBot
 		{
 			try
 			{
-				string url = string.Format("http://oxforddictionaries.com/search/english/?direct=1&multi=1&q={0}", HttpUtility.UrlEncode(term));
+				string url = string.Format("http://www.oxforddictionaries.com/search/english/?direct=1&multi=1&q={0}", HttpUtility.UrlEncode(term));
 				WebClient client = new WebClient();
 				client.Encoding = Encoding.UTF8;
 				string content = client.DownloadString(url);
-				Match match = Regex.Match(content, "<a href=\"http://oxforddictionaries.com/words/key-to-pronunciation\"> (/.+?/)</a>");
+				Match match = Regex.Match(content, "<a href=\"http://www.oxforddictionaries.com/words/key-to-pronunciation\"> (/.+?/)</a>");
 				if (!match.Success)
 					return false;
 				output = match.Groups[1].Value;
