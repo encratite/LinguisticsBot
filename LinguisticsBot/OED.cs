fc13f65e750ec46c4ec3b0da6e48f661b9a967c4
void <git.email.address@googlemail.com>
2014-01-25 21:12:47 +0100

    OED fix

--- LinguisticsBot/OED.cs
+++ LinguisticsBot/OED.cs
@@ -15,10 +15,10 @@ namespace LinguisticsBot
 				WebClient client = new WebClient();
 				client.Encoding = Encoding.UTF8;
 				string content = client.DownloadString(url);
-				Match match = Regex.Match(content, "<a href=\"http://www.oxforddictionaries.com/words/key-to-pronunciation\"> (/.+?/)</a>");
+				Match match = Regex.Match(content, "<a href=\"http://www.oxforddictionaries.com/words/key-to-pronunciation\">Pronunciation: <\\/a> \\/(.+)");
 				if (!match.Success)
 					return false;
-				output = match.Groups[1].Value;
+				output = string.Format("/{0}/", match.Groups[1].Value);
 				return true;
 			}
 			catch (WebException)
